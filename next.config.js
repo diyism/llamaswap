f933346c0d5e299a5567c3c693d4720f6f34e16f
Malcolm Ke Win <kexianbin@diyism.com>
2024-09-04 17:50:59 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,29 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+	    // 对客户端构建进行优化
+	    if (!isServer) {
+	      config.optimization.splitChunks = {
+	        chunks: 'all',
+	        maxInitialRequests: Infinity,
+	        minSize: 0,
+	        cacheGroups: {
+	          vendor: {
+	            test: /[\\/]node_modules[\\/]/,
+	            name(module) {
+	              // 获取包名
+	              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+	              // npm 包名可能包含 @，我们需要替换掉
+	              return `npm.${packageName.replace('@', '')}`;
+	            },
+	          },
+	        },
+	      };
+	    }
+	    return config;
+        },
+
 	staticPageGenerationTimeout: 1000,
 	images: {
 		unoptimized: true, // for cloudflare pages
